610786e4145ca77f0d2a70f79131a7de0d0ebc54
Ignacio Mondino <ignacio.mondino@gmail.com>
2018-06-13 00:26:01 -0300

    MAny changes, aded coments and fixed typos

--- osx_setup.sh
+++ osx_setup.sh
@@ -1,26 +1,46 @@
 
 #Install Homebrew
+echo "Installing Homebrew"
+echo ""
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #Install Software
+echo ""
+echo "Installing software"
+echo ""
 brew install ansible terraform packer awscli amazon-ecs-cli aws-elasticbeanstalk git
 
+echo ""
+echo "Installing cask software"
+echo
 #Install cask sofware
 brew cask install atom spotify virtualbox vagrant docker
 
+echo ""
+echo "Installing some plugins"
+echo ""
 #Install plugins
 vagrant plugin install vagrant-hostsupdater
 apm install multi-cursor script
 
 #Set up repos
-if [ ! -d "git" ]; then
+if [ ! -d ~/git ]; then
   mkdir ~/git
 fi
 
-pushd ~/git
+echo ""
+echo "Setting up env"
+echo ""
+#Install  bash osx_setup
+# https://github.com/imondino/dotfiles
+
+pushd ~/git > /dev/null
 
 if [ ! -d "dotfiles" ]; then
-  git clone https://github.com/imondino/osx_setup.git
+  echo ""
+  echo "Clonning dotfiles repo"
+  echo ""
+  git clone https://github.com/imondino/dotfiles.git
 fi
 
 ln -sf git/dotfiles/.aliases ~
@@ -30,4 +50,8 @@ ln -sf git/dotfiles/.bashrc ~
 ln -sf git/dotfiles/.functions ~
 ln -sf git/dotfiles/.gitconfig ~
 ln -sf git/dotfiles/.vimrc ~
-popd
+popd > /dev/null
+
+echo ""
+echo "Complete! Remember to import the custom Solarized theme."
+echo ""
