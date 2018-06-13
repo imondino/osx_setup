14e4386e4f831663b0ec901f65535cb5a7d71ea5
Ignacio Mondino <ignacio.mondino@gmail.com>
2018-06-12 23:40:51 -0300

    Added profile configuration and fixes

--- osx_setup.sh
+++ osx_setup.sh
@@ -1,6 +1,6 @@
 
 #Install Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #Install Software
 brew install ansible terraform packer awscli amazon-ecs-cli aws-elasticbeanstalk git
@@ -10,17 +10,17 @@ brew cask install atom spotify virtualbox vagrant docker
 
 #Install plugins
 vagrant plugin install vagrant-hostsupdater
-apm install multi-curior script
+apm install multi-cursor script
 
 #Set up repos
 if [ ! -d "git" ]; then
   mkdir ~/git
 fi
 
-pushd git
+pushd ~/git
 
 if [ ! -d "dotfiles" ]; then
-  git clone git@github.com:imondino/dotfiles.git
+  git clone https://github.com/imondino/osx_setup.git
 fi
 
 ln -sf git/dotfiles/.aliases ~
@@ -31,3 +31,9 @@ ln -sf git/dotfiles/.functions ~
 ln -sf git/dotfiles/.gitconfig ~
 ln -sf git/dotfiles/.vimrc ~
 popd
+
+if [ ! -f "com.apple.Terminal.plist" ]; then
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install > ~/Library/Preferences/com.apple.Terminal.plist
+else
+  cp com.apple.Terminal.plist ~/Library/Preferences/com.apple.Terminal.plist
+fi
